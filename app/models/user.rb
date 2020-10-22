83a053a180bb9918f22af9b4e574ef23e8c976f3
scotth527 <scottyh527@gmail.com>
2020-10-22 16:51:41 -0400

    working on user and events associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :events
 end

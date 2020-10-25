182f4929265e5a1631e330ab726af5ebbc0a6bf0
scotth527 <scottyh527@gmail.com>
2020-10-25 16:07:16 -0400

    testing

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,4 +1,4 @@
 class Rsvp < ApplicationRecord
-    belongs_to :invitee, class_name: "User", foreign_key: :user_id
-    belongs_to :event_id, class_name: "Event", foreign_key: :event_id
+    belongs_to :invitee, class_name: "User", foreign_key: 'user_id'
+    belongs_to :event_id, class_name: "Event", foreign_key: 'event_id'
 end

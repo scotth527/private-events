34817f073826db3d2caa5ffc6b6c3e2c90d99e9b
scotth527 <scottyh527@gmail.com>
2020-12-03 20:20:36 -0500

    working on debugging the date parsing for the separated event lists

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+    validates :date, presence: true
+
     belongs_to :creator, class_name: "User", foreign_key: :user_id
     has_many :rsvps
     has_many :attendees, through: :rsvps, :foreign_key => :attendee_id
